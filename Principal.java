e0ce594d0fdc0909b3fa52872234f7e4f93b1bc5
JesusGutierrez89 <3687690@alu.murciaeduca.es>
2024-02-01 19:37:39 +0100

    Archivo modificado para mostar fecha y hora actual

--- Principal.java
+++ Principal.java
@@ -1,8 +1,18 @@
-public class Principal{
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Principal {
 	
-	 public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-	
-	}
-		
+    public static void main(String[] args) {
+        String FechaAhora = fechaHora();
+        System.out.println("La fecha actual con su hora es " + FechaAhora);
+    }
+
+    public static String fechaHora() {
+        LocalDateTime tiempo = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String fechaFormateada = tiempo.format(formato);
+
+        return fechaFormateada;
+    }
 }
