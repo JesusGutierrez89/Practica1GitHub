b3c7f1eab23fdca3c57f01c40cf59ae6d796a5d5
JesusGutierrez89 <3687690@alu.murciaeduca.es>
2024-02-02 13:01:48 +0100

    Creacion de clase Usuario y modificacion de el main

--- Principal.java
+++ Principal.java
@@ -1,18 +1,46 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Principal {
-	
     public static void main(String[] args) {
-        String FechaAhora = fechaHora();
-        System.out.println("La fecha actual con su hora es " + FechaAhora);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Dime el tamaño de los usuarios: ");
+        int tamano = sc.nextInt();
+        sc.nextLine();  
+
+        Usuario[] usuarios = arrayUsuario(tamano);
+        
+        System.out.println();
+        System.out.println("Usuario");
+        for (Usuario usuario : usuarios) {
+            System.out.print("El Nombre: " + usuario.nombre);
+            System.out.print(" Los Apellidos: " + usuario.apellidos);
+            System.out.print(" El Email: " + usuario.email);
+        }
+
+       
+        sc.close();
     }
 
-    public static String fechaHora() {
-        LocalDateTime tiempo = LocalDateTime.now();
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String fechaFormateada = tiempo.format(formato);
+    public static Usuario[] arrayUsuario(int tamano) {
+        Scanner sc = new Scanner(System.in);
+        
+        Usuario[] usuarios = new Usuario[tamano];
 
-        return fechaFormateada;
+        for (int i = 0; i < tamano; i++) {
+            System.out.print("Dime el nombre: ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Dime el apellidos: ");
+            String apellidos = sc.nextLine();
+
+            System.out.print("Dime el email: ");
+            String email = sc.nextLine();
+
+            usuarios[i] = new Usuario(nombre, apellidos, email);
+        }
+
+        return usuarios;
     }
 }
+
